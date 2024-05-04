dd93252d94fe5b3e34c9df2feccc440e765e4a03
joelgroomer <joelgroomer@users.noreply.github.com>
2024-05-03 22:17:45 -0700

    Clamp player to screen

--- alien_attack/scripts/player.gd
+++ alien_attack/scripts/player.gd
@@ -18,4 +18,27 @@ func _physics_process(delta):
 		velocity.y = -speed
 	if Input.is_action_pressed("move_down"):
 		velocity.y = speed
+		
 	move_and_slide()
+	
+	# Clamping player's position inside the screen
+	# 3 ways to do it
+	var screen_size = get_viewport_rect().size
+	
+	# 1.
+	#if global_position.x < 0:
+		#global_position.x = 0
+	#if global_position.x > screen_size.x:
+		#global_position.x = screen_size.x
+	#if global_position.y < 0:
+		#global_position.y = 0
+	#if global_position.y > screen_size.y:
+		#global_position.y = screen_size.y
+	
+	# 2.
+	#global_position.x = clampf(global_position.x, 0, screen_size.x)
+	#global_position.y = clampf(global_position.y, 0, screen_size.y)
+	
+	# 3.
+	global_position = global_position.clamp(Vector2(0,0), screen_size)
+	
