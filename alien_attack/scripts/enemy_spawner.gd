c94f4e03a34773e56eea47b5387c266bc517b264
joelgroomer <joelgroomer@users.noreply.github.com>
2024-05-04 22:13:30 -0700

    Add PathEnemy to EnemySpawner

--- alien_attack/scripts/enemy_spawner.gd
+++ alien_attack/scripts/enemy_spawner.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
 signal enemy_spawned(enemy_instance)
+signal path_enemy_spawned(path_enemy_instance)
 
 var enemy_scene = preload("res://scenes/enemy.tscn")
+var path_enemy_scene = preload("res://scenes/path_enemy.tscn")
 @onready var spawn_positions = $SpawnPositions
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -25,3 +28,11 @@ func spawn_enemy():
 	enemy_instance.global_position = random_spawn_position.global_position
 	emit_signal("enemy_spawned", enemy_instance)
 	#add_child(enemy_instance)
+
+
+func _on_path_enemy_timer_timeout():
+	spawn_path_enemy()
+
+func spawn_path_enemy():
+	var path_enemy_instance = path_enemy_scene.instantiate()
+	emit_signal("path_enemy_spawned", path_enemy_instance)
