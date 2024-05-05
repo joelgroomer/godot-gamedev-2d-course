edff388430f91a4ac1f0516db634ed2b24658a4b
joelgroomer <joelgroomer@users.noreply.github.com>
2024-05-04 22:47:26 -0700

    Configure quit and reset actions

--- martian_mike/scripts/level.gd
+++ martian_mike/scripts/level.gd
@@ -8,4 +8,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+	elif Input.is_action_just_pressed("reset"):
+		get_tree().reload_current_scene()
