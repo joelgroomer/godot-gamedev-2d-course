09e146abf54d40edd563db43815b1b26cee23754
joelgroomer <joelgroomer@users.noreply.github.com>
2024-05-05 22:23:07 -0700

    Add Player

---
+++ martian_mike/scripts/player.gd
@@ -0,0 +1,43 @@
+extends CharacterBody2D
+
+
+@export var speed = 125
+@export var jump_force = -200.0
+@export var gravity = 400
+
+@onready var animated_sprite = $AnimatedSprite2D
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		if velocity.y > 500:
+			velocity.y = 500
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_force
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction:
+		animated_sprite.flip_h = (direction == -1)
+		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+
+	move_and_slide()
+	update_animations(direction)
+	
+func update_animations(direction):
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		if velocity.y < 0:
+			animated_sprite.play("jump")
+		else:
+			animated_sprite.play("fall")
