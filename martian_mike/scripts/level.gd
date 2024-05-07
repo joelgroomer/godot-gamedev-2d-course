4575b8cad4c5542ede28288661142cec3224324b
joelgroomer <joelgroomer@users.noreply.github.com>
2024-05-06 21:33:06 -0700

    Add terrain tileset, automate touched_player signal connections

--- martian_mike/scripts/level.gd
+++ martian_mike/scripts/level.gd
@@ -3,20 +3,23 @@ extends Node2D
 @onready var start_position = $StartPosition
 @onready var player = $Player
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	var traps = get_tree().get_nodes_in_group("traps")
+	for trap in traps:
+		print(trap)
+		trap.connect("touched_player", _on_trap_touched_player)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
 	elif Input.is_action_just_pressed("reset"):
 		get_tree().reload_current_scene()
 
 
-func _on_deathzone_body_entered(body):
+func _on_deathzone_body_entered(_body):
 	reset_player()
 
 func _on_trap_touched_player():
