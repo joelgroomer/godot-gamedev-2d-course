d0c73ccbda14c21a92497a2cf70f060a6fc047ed
joelgroomer <joelgroomer@users.noreply.github.com>
2024-05-06 20:24:04 -0700

    Add Saw and SpikeBall traps

--- martian_mike/scripts/level.gd
+++ martian_mike/scripts/level.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var start_position = $StartPosition
+@onready var player = $Player
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,6 +17,11 @@ func _process(delta):
 
 
 func _on_deathzone_body_entered(body):
-	body.velocity = Vector2.ZERO
-	body.global_position = start_position.global_position
-	
+	reset_player()
+
+func _on_trap_touched_player():
+	reset_player()
+
+func reset_player():
+	player.velocity = Vector2.ZERO
+	player.global_position = start_position.global_position
