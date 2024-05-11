8685d08f60c0da6b526fc36ae33ab99224b22d70
joelgroomer <joelgroomer@users.noreply.github.com>
2024-05-10 23:04:16 -0700

    Fix 'Signal is already connected' error messages

--- martian_mike/scripts/level.gd
+++ martian_mike/scripts/level.gd
@@ -18,8 +18,10 @@ func _ready():
 	for trap in traps:
 		trap.connect("touched_player", _on_trap_touched_player)
 	
-	exit.body_entered.connect(_on_exit_body_entered)
-	death_zone.body_entered.connect(_on_deathzone_body_entered)
+	if !exit.is_connected("body_entered", _on_exit_body_entered):
+		exit.body_entered.connect(_on_exit_body_entered)
+	if !death_zone.is_connected("body_entered", _on_deathzone_body_entered):
+		death_zone.body_entered.connect(_on_deathzone_body_entered)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
@@ -40,8 +42,12 @@ func reset_player():
 	player.global_position = start.get_spawn_position()
 
 func _on_exit_body_entered(body):
+	#print(next_level.resource_name)
+	#print("helloooooo")
 	if body is Player && next_level != null:
 		exit.animate()
 		player.active = false
 		await get_tree().create_timer(1.5).timeout
+		exit.queue_free()
+		death_zone.queue_free()
 		get_tree().change_scene_to_packed(next_level)
